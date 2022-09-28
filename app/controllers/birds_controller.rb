b83be7aee4fa294041efd8861d2e23de24aa11e4
principalkelvo <principalkelvo@gmail.com>
2022-09-28 08:47:07 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
-
+  # disable the wrap parameters feature in an individual controller
+  wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -21,5 +22,20 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+    # POST /birds
+    def create
+      bird = Bird.create(bird_params)
+      render json: bird, status: :created
+    end
+  
+    # other controller actions here
+  
+    private
+    # all methods below here are private
+  
+    def bird_params
+      params.permit(:name, :species)
+    end
 
 end
